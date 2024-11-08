d46813b072c73220460ebe5677d58638229be584
duongdv <duong.dv@htigroup.vn>
2024-11-08 17:31:30 +0700

    add user controller

--- internal/controller/user.controller.go
+++ internal/controller/user.controller.go
@@ -1,4 +1,19 @@
 package controller
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+type UserController struct {}
 
+func NewUserController() *UserController  {
+	return &UserController{}
+}
+
+func (uc *UserController) GetUserById(c *gin.Context)  {
+	name := c.DefaultQuery("name","duongdv")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Suceess!",
+	})
+}
